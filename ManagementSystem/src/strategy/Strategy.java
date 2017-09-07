687306f18b8fa4032638b30515db1ee4e342ce2a
LucasSSales <ldss@ic.ufal.br>
2017-09-07 17:36:54 -0300

    Add files via upload

--- ManagementSystem/src/strategy/Strategy.java
+++ ManagementSystem/src/strategy/Strategy.java
@@ -2,12 +2,41 @@ package Strategy;
 
 import java.util.ArrayList;
 
-public class Strategy {
-	//add elements on lists
-	public <T> ArrayList<T> add(ArrayList<T> a, T t){
+import allocation.Allocation;
+import users.User;
+
+public class Strategy<T> {
+	
+	public ArrayList<T> add(ArrayList<T> a, T t){
 		if(a == null) 
 			a = new ArrayList<T>();
 		a.add(t);
 		return a;
 	}
+	
+	public Object search (ArrayList<T> a, String key) {
+		T found = null;
+		if(a!=null) {
+			for (T t : a) {
+				try {	
+					Allocation x = (Allocation) t;
+					if(x.getName().equals(key) || x.getCode().equals(key)) {
+						found = (T) x;
+						break;
+					}
+											
+				}catch(ClassCastException exception) {	
+					User x = (User) t;
+					if(x.getName().equals(key) || x.getID().equals(key)) {
+						found = (T) x;
+						break;
+					}	
+				}			
+			}
+		}
+		return found;
+	}
+	
+	
+	// java.lang.ClassCastException (nao casta user p allocation)
 }
