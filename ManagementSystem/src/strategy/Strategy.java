fc9cfecd154be4e8355704aff1112df0fe606a0c
LucasSSales <ldss@ic.ufal.br>
2017-09-09 23:02:57 -0300

    Update Strategy.java

--- ManagementSystem/src/strategy/Strategy.java
+++ ManagementSystem/src/strategy/Strategy.java
@@ -49,16 +49,5 @@ public class Strategy<T> {
 			}
 		}
 		return found;
-	}
-	
-	public void delete (ArrayList<T> a, T t) {
-		a.remove(a.indexOf(t));
-		System.out.println("Removed from list");
-	}
-	
-	public void delete (ArrayList<T> a, int index) {
-		a.remove(index);
-		System.out.println("Removed from list");
-	}
-	
+	}	
 }
